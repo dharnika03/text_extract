399d47fd37f10860a551fbc70172c01340790082
Dharnika B <79350916+dharnika03@users.noreply.github.com>
2024-06-26 19:23:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from pypdf import PdfReader
-
-
 import urllib.request
+
+app = Flask(__name__)
 
 
 def download_file(url, filename=None):
@@ -28,32 +28,31 @@ def download_file(url, filename=None):
     return filename
 
 
-app = Flask(__name__)
-
-
-@app.route("/read_text", methods=["POST"])
+@app.route("/read_text", methods=["GET"])
 def download_from_url():
     """
-    Accepts a URL as POST data and calls the download_file function.
+    Accepts a URL as a query parameter and calls the download_file function.
     """
-    data = request.get_json()
-    url = data['url']
+    url = request.args.get('url')
     if not url:
-        return "Missing URL in request body!", 400
+        return "Missing URL parameter!", 400
 
-    downloaded_file = download_file(url)
-    print(f"Downloaded file: {downloaded_file}")
-    reader = PdfReader(downloaded_file)
-    print(len(reader.pages))
-    page = reader.pages[0]
-    text = page.extract_text()
-    result = ""
-    for char in text:   
-        if char.isalnum() or char.isspace():
-            result += char
-    response = {'body': result.replace("\n", "")}
-    return jsonify(response)
+    try:
+        downloaded_file = download_file(url)
+        print(f"Downloaded file: {downloaded_file}")
+        reader = PdfReader(downloaded_file)
+        print(len(reader.pages))
+        page = reader.pages[0]
+        text = page.extract_text()
+        result = ""
+        for char in text:
+            if char.isalnum() or char.isspace():
+                result += char
+        response = {'body': result.replace("\n", "")}
+        return jsonify(response)
+    except Exception as e:
+        return str(e), 500
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
